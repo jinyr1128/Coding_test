b40a4a1f40bd233e7ac6e6420ddba2810b1a775c
yullgii <142703323+jinyr1128@users.noreply.github.com>
2023-11-09 17:48:05 +0900

    [level 2] Title: 피보나치 수, Time: 2.81 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int a = 0;
+        int b = 1;
+        for (int i = 2; i <= n; i++) {
+            int c = (a + b) % 1234567;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+}
