f3cc243eb8be37b607f9848f174806829ce85033
yullgii <142703323+jinyr1128@users.noreply.github.com>
2023-11-30 10:22:39 +0900

    [level 1] Title: 체육복, Time: 1.14 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int[] students = new int[n];
+        
+        for (int l : lost) 
+            students[l-1]--;
+        for (int r : reserve) 
+            students[r-1]++;
+        
+        for (int i = 0; i < students.length; i++) {
+            if(students[i] < 0) {
+                if(i-1>=0 && students[i-1] > 0) {
+                    students[i]++;
+                    students[i-1]--;
+                } else if(i+1<students.length && students[i+1] > 0) {
+                    students[i]++;
+                    students[i+1]--;
+                }
+            }
+        }
+        
+        return (int) Arrays.stream(students).filter(i -> i >= 0).count();
+    }
+}
