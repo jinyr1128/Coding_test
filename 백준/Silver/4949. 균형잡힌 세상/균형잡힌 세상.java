d20a95b66706fae8e8426af61b3f7723e5ec289e
yullgii <142703323+jinyr1128@users.noreply.github.com>
2024-05-02 23:04:47 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 196 ms, Memory: 18152 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            String line = br.readLine();
+            if (line.equals(".")) { 
+                break;
+            }
+            sb.append(isBalanced(line) ? "yes\n" : "no\n");
+        }
+        System.out.println(sb);
+    }
+
+    public static boolean isBalanced(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch == '(' || ch == '[') {
+                stack.push(ch);
+            } else if (ch == ')') {
+                if (stack.isEmpty() || stack.pop() != '(') {
+                    return false;
+                }
+            } else if (ch == ']') {
+                if (stack.isEmpty() || stack.pop() != '[') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
