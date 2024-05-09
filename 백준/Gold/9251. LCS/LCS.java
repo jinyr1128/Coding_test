1d91a63937f08b1ee900fab4e037531e8ff3943a
yullgii <142703323+jinyr1128@users.noreply.github.com>
2024-05-09 15:56:52 +0900

    [Gold V] Title: LCS, Time: 152 ms, Memory: 18248 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] s1 = br.readLine().toCharArray();
+        char[] s2 = br.readLine().toCharArray();
+
+        int n = s1.length;
+        int m = s2.length;
+
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s1[i - 1] == s2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        System.out.println(dp[n][m]);
+    }
+}
