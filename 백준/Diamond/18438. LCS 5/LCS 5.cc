f390aca6b1a3b49b9be5118fb417c2a096f43479
yullgii <142703323+jinyr1128@users.noreply.github.com>
2024-05-14 11:53:22 +0900

    [Diamond I] Title: LCS 5, Time: 304 ms, Memory: 2164 KB -BaekjoonHub

--- 백준/Diamond/18438. LCS 5/LCS 5.cc
+++ 백준/Diamond/18438. LCS 5/LCS 5.cc
@@ -1,51 +1,77 @@
 #include <iostream>
-#include <algorithm>
-#include <string>
 #include <vector>
+#include <string>
+#include <algorithm>
 
-std::string S, T, lcs;
-void hirschberg(int u, int d, int l, int r) {
-	if (u > d) return;
-	if (u == d) { // only one row
-		for (int j = l; j <= r; ++j) {
-			if (S[u] == T[j]) {
-				lcs += S[u];
-				break;
-			}
-		}
-		return;
-	}
-	int mid = u + d >> 1;
-	std::vector<int> L(r - l + 3), R(r - l + 3), pre(r - l + 3);
-	for (int i = u; i <= mid; ++i) {
-		for (int j = l; j <= r; ++j) {
-			if (S[i] == T[j]) L[j - l + 1] = pre[j - l] + 1;
-			else L[j - l + 1] = std::max(L[j - l], pre[j - l + 1]);
-		}
-		pre = L;
-	}
-	std::fill(pre.begin(), pre.end(), 0);
-	for (int i = d; i > mid; --i) {
-		for (int j = r; j >= l; --j) {
-			if (S[i] == T[j]) R[j - l + 1] = pre[j - l + 2] + 1;
-			else R[j - l + 1] = std::max(R[j - l + 2], pre[j - l + 1]);
-		}
-		pre = R;
-	}
-	int max = -1, idx = 0;
-	for (int i = l; i <= r + 1; ++i) {
-		if (L[i - l] + R[i - l + 1] > max) {
-			max = L[i - l] + R[i - l + 1];
-			idx = i;
-		}
-	}
-	hirschberg(u, mid, l, idx - 1);
-	hirschberg(mid + 1, d, idx, r);
-}
+std::string hirschberg(const std::string& S, const std::string& T);
 
 int main() {
-	std::cin >> S >> T;
-	hirschberg(0, S.length() - 1, 0, T.length() - 1);
-	std::cout << lcs.length() << '\n';
-	std::cout << lcs;
+    std::string S, T;
+    std::cin >> S >> T;
+    
+    std::string lcs = hirschberg(S, T);
+    std::cout << lcs.length() << '\n';
+    std::cout << lcs << '\n';
+
+    return 0;
+}
+
+std::vector<int> lcsLength(const std::string& X, const std::string& Y) {
+    int m = X.length();
+    int n = Y.length();
+    std::vector<int> current(n + 1, 0);
+    std::vector<int> previous(n + 1, 0);
+
+    for (int i = 1; i <= m; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            if (X[i - 1] == Y[j - 1]) {
+                current[j] = previous[j - 1] + 1;
+            } else {
+                current[j] = std::max(previous[j], current[j - 1]);
+            }
+        }
+        std::swap(current, previous);
+    }
+    return previous;
+}
+
+std::string hirschberg(const std::string& S, const std::string& T) {
+    int m = S.length();
+    int n = T.length();
+
+    if (m == 0) {
+        return "";
+    } else if (m == 1) {
+        if (T.find(S) != std::string::npos) {
+            return S;
+        } else {
+            return "";
+        }
+    } else if (n == 0) {
+        return "";
+    } else if (n == 1) {
+        if (S.find(T) != std::string::npos) {
+            return T;
+        } else {
+            return "";
+        }
+    }
+
+    int mid = m / 2;
+    std::vector<int> L1 = lcsLength(S.substr(0, mid), T);
+    std::vector<int> L2 = lcsLength(std::string(S.rbegin(), S.rbegin() + (m - mid)), std::string(T.rbegin(), T.rend()));
+
+    int k = 0;
+    int max = 0;
+    for (int j = 0; j <= n; ++j) {
+        if (L1[j] + L2[n - j] > max) {
+            max = L1[j] + L2[n - j];
+            k = j;
+        }
+    }
+
+    std::string lcsLeft = hirschberg(S.substr(0, mid), T.substr(0, k));
+    std::string lcsRight = hirschberg(S.substr(mid), T.substr(k));
+
+    return lcsLeft + lcsRight;
 }
