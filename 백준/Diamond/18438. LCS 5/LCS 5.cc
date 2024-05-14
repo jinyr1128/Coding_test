7201d42ef8f4c203840c6d4603acd82c8742b405
yullgii <142703323+jinyr1128@users.noreply.github.com>
2024-05-14 15:32:34 +0900

    [Diamond I] Title: LCS 5, Time: 284 ms, Memory: 2164 KB -BaekjoonHub

--- 백준/Diamond/18438. LCS 5/LCS 5.cc
+++ 백준/Diamond/18438. LCS 5/LCS 5.cc
@@ -1,77 +1,43 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <algorithm>
+using namespace std;
 
-std::string hirschberg(const std::string& S, const std::string& T);
+vector<int> lcsLen(const string &X, const string &Y) {
+    int n = Y.size();
+    vector<int> curr(n + 1), prev(n + 1);
+    for (char x : X) {
+        swap(curr, prev);
+        for (int j = 1; j <= n; ++j)
+            curr[j] = (x == Y[j - 1]) ? prev[j - 1] + 1 : max(prev[j], curr[j - 1]);
+    }
+    return curr;
+}
 
-int main() {
-    std::string S, T;
-    std::cin >> S >> T;
+string hirschberg(const string &S, const string &T) {
+    int m = S.size(), n = T.size();
+    if (m == 0) return "";
+    if (m == 1) return T.find(S) != string::npos ? S : "";
+    if (n == 0) return "";
+    if (n == 1) return S.find(T) != string::npos ? T : "";
     
-    std::string lcs = hirschberg(S, T);
-    std::cout << lcs.length() << '\n';
-    std::cout << lcs << '\n';
-
-    return 0;
-}
-
-std::vector<int> lcsLength(const std::string& X, const std::string& Y) {
-    int m = X.length();
-    int n = Y.length();
-    std::vector<int> current(n + 1, 0);
-    std::vector<int> previous(n + 1, 0);
-
-    for (int i = 1; i <= m; ++i) {
-        for (int j = 1; j <= n; ++j) {
-            if (X[i - 1] == Y[j - 1]) {
-                current[j] = previous[j - 1] + 1;
-            } else {
-                current[j] = std::max(previous[j], current[j - 1]);
-            }
-        }
-        std::swap(current, previous);
-    }
-    return previous;
-}
-
-std::string hirschberg(const std::string& S, const std::string& T) {
-    int m = S.length();
-    int n = T.length();
-
-    if (m == 0) {
-        return "";
-    } else if (m == 1) {
-        if (T.find(S) != std::string::npos) {
-            return S;
-        } else {
-            return "";
-        }
-    } else if (n == 0) {
-        return "";
-    } else if (n == 1) {
-        if (S.find(T) != std::string::npos) {
-            return T;
-        } else {
-            return "";
-        }
-    }
-
     int mid = m / 2;
-    std::vector<int> L1 = lcsLength(S.substr(0, mid), T);
-    std::vector<int> L2 = lcsLength(std::string(S.rbegin(), S.rbegin() + (m - mid)), std::string(T.rbegin(), T.rend()));
-
-    int k = 0;
-    int max = 0;
-    for (int j = 0; j <= n; ++j) {
+    auto L1 = lcsLen(S.substr(0, mid), T);
+    auto L2 = lcsLen(string(S.rbegin(), S.rbegin() + (m - mid)), string(T.rbegin(), T.rend()));
+    
+    int k = 0, max = 0;
+    for (int j = 0; j <= n; ++j)
         if (L1[j] + L2[n - j] > max) {
             max = L1[j] + L2[n - j];
             k = j;
         }
-    }
+    return hirschberg(S.substr(0, mid), T.substr(0, k)) + hirschberg(S.substr(mid), T.substr(k));
+}
 
-    std::string lcsLeft = hirschberg(S.substr(0, mid), T.substr(0, k));
-    std::string lcsRight = hirschberg(S.substr(mid), T.substr(k));
-
-    return lcsLeft + lcsRight;
+int main() {
+    string S, T;
+    cin >> S >> T;
+    string lcs = hirschberg(S, T);
+    cout << lcs.size() << '\n' << lcs << '\n';
+    return 0;
 }
