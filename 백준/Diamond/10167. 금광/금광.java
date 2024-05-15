9eb094edc42fa267807f9ed4216da07d41a498fd
yullgii <142703323+jinyr1128@users.noreply.github.com>
2024-05-15 16:28:59 +0900

    [Diamond V] Title: 금광, Time: 1740 ms, Memory: 304252 KB, Score: 100 point -BaekjoonHub

--- 백준/Diamond/10167. 금광/금광.java
+++ 백준/Diamond/10167. 금광/금광.java
@@ -1,156 +1,126 @@
 import java.io.*;
 import java.util.*;
 
-class GoldMine implements Comparable<GoldMine> {
-    int y, x, w;
-
-    public GoldMine(int x, int y, int w) {
-        this.y = y;
-        this.x = x;
-        this.w = w;
-    }
-
-    @Override
-    public int compareTo(GoldMine o) {
-        return this.y - o.y;
-    }
-}
-
-class SegmentTreeNode {
-    long rightVal, leftVal, maxVal, sum;
-
-    public SegmentTreeNode(long rightVal, long leftVal, long maxVal, long sum) {
-        this.rightVal = rightVal;
-        this.leftVal = leftVal;
-        this.maxVal = maxVal;
-        this.sum = sum;
-    }
-}
-
 public class Main {
 
+    // 금광 정보를 저장하는 클래스
+    static class Mine {
+        int x, y, weight;
+
+        Mine(int x, int y, int weight) {
+            this.x = x;
+            this.y = y;
+            this.weight = weight;
+        }
+    }
+
+    // 세그먼트 트리 노드를 저장하는 클래스
+    static class SegmentTreeNode {
+        long leftMax, rightMax, max, sum;
+
+        SegmentTreeNode() {}
+
+        SegmentTreeNode(long value) {
+            this.leftMax = this.rightMax = this.max = this.sum = value;
+        }
+
+        SegmentTreeNode(long leftMax, long rightMax, long max, long sum) {
+            this.leftMax = leftMax;
+            this.rightMax = rightMax;
+            this.max = max;
+            this.sum = sum;
+        }
+    }
+
+    static int N, uniqueXCount;
+    static ArrayList<Integer> xList = new ArrayList<>();
+    static HashMap<Integer, Integer> xCoordinateMap = new HashMap<>();
+    static Mine[] mines;
     static SegmentTreeNode[] segmentTree;
-    static int[] xCoordinates;
-    static long maxGold = 0;
-    static int treeSize = 2, uniqueXCount, uniqueYCount;
-    static final long MIN_VALUE = (long) -1e15;
-
-    // 세그먼트 트리 구성
-    static void constructSegmentTree() {
-        for (int i = treeSize / 2 - 1; i > 0; i--) {
-            long rightVal, leftVal, maxVal, sum;
-            rightVal = Math.max(segmentTree[i * 2 + 1].sum + segmentTree[i * 2].rightVal, segmentTree[i * 2 + 1].rightVal);
-            leftVal = Math.max(segmentTree[i * 2].sum + segmentTree[i * 2 + 1].leftVal, segmentTree[i * 2].leftVal);
-            maxVal = Math.max(segmentTree[i * 2].maxVal, segmentTree[i * 2 + 1].maxVal);
-            maxVal = Math.max(segmentTree[i * 2].rightVal + segmentTree[i * 2 + 1].leftVal, maxVal);
-            sum = segmentTree[i * 2].sum + segmentTree[i * 2 + 1].sum;
-            segmentTree[i] = new SegmentTreeNode(rightVal, leftVal, maxVal, sum);
-        }
-    }
-
-    // 세그먼트 트리 업데이트
-    static void updateSegmentTree(int index, int value) {
-        index += treeSize / 2;
-        segmentTree[index].leftVal += value;
-        segmentTree[index].rightVal += value;
-        segmentTree[index].sum += value;
-        segmentTree[index].maxVal += value;
-        while (index > 1) {
-            index /= 2;
-            long rightVal, leftVal, maxVal, sum;
-            rightVal = Math.max(segmentTree[index * 2 + 1].sum + segmentTree[index * 2].rightVal, segmentTree[index * 2 + 1].rightVal);
-            leftVal = Math.max(segmentTree[index * 2].sum + segmentTree[index * 2 + 1].leftVal, segmentTree[index * 2].leftVal);
-            maxVal = Math.max(segmentTree[index * 2].maxVal, segmentTree[index * 2 + 1].maxVal);
-            maxVal = Math.max(segmentTree[index * 2].rightVal + segmentTree[index * 2 + 1].leftVal, maxVal);
-            sum = segmentTree[index * 2].sum + segmentTree[index * 2 + 1].sum;
-            segmentTree[index] = new SegmentTreeNode(rightVal, leftVal, maxVal, sum);
-        }
-    }
-
-    // 세그먼트 트리 초기화
-    static void initializeSegmentTree() {
-        segmentTree = new SegmentTreeNode[treeSize];
-        for (int i = 0; i < treeSize / 2; i++) {
-            if (i < uniqueXCount) {
-                segmentTree[treeSize / 2 + i] = new SegmentTreeNode(0, 0, 0, 0);
-            } else {
-                segmentTree[treeSize / 2 + i] = new SegmentTreeNode(MIN_VALUE, MIN_VALUE, MIN_VALUE, 0);
-            }
-        }
-    }
-
-    // 세그먼트 트리에서 최대값 가져오기
-    static long getMaxValue() {
-        long max = 0;
-        max = Math.max(segmentTree[1].leftVal, segmentTree[1].rightVal);
-        max = Math.max(segmentTree[1].maxVal, max);
-        max = Math.max(segmentTree[1].sum, max);
-        return max;
-    }
+    static ArrayList<ArrayList<Mine>> mineLists = new ArrayList<>();
+    static final SegmentTreeNode DUMMY_NODE = new SegmentTreeNode();
+    static long maxGold;
 
     public static void main(String[] args) throws IOException {
-        Main mainClass = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        xCoordinates = new int[N];
+        N = Integer.parseInt(br.readLine());
+        mines = new Mine[N];
 
-        ArrayList<GoldMine> goldMines = new ArrayList<>();
+        // 입력을 받아 금광 정보를 저장하고 x 좌표 리스트
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
-            xCoordinates[i] = x;
-            goldMines.add(new GoldMine(x, Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
-        }
-        Arrays.sort(xCoordinates);
-        int previous = Integer.MIN_VALUE;
-        HashMap<Integer, Integer> xCoordinateIndex = new HashMap<>();
-        uniqueXCount = 0;
-        for (int i = 0; i < xCoordinates.length; i++) {
-            if (xCoordinates[i] != previous) {
-                xCoordinateIndex.put(xCoordinates[i], ++uniqueXCount);
-                previous = xCoordinates[i];
-            }
-        }
-        Collections.sort(goldMines);
-        previous = Integer.MIN_VALUE;
-        uniqueYCount = 0;
-        for (int i = 0; i < N; i++) {
-            GoldMine goldMine = goldMines.get(i);
-            if (goldMine.y == previous) {
-                goldMine.y = uniqueYCount;
-            } else {
-                previous = goldMine.y;
-                goldMine.y = ++uniqueYCount;
-            }
-            goldMine.x = xCoordinateIndex.get(goldMine.x);
+            int y = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            mines[i] = new Mine(x, y, weight);
+            xList.add(x);
         }
 
-        while (true) {
-            if (treeSize >= uniqueXCount) {
-                treeSize *= 2;
-                break;
+        // x 좌표를 정렬하고 유일한 값들에 대한 매핑
+        xList.sort(Comparator.naturalOrder());
+        for (Integer x : xList) {
+            if (!xCoordinateMap.containsKey(x)) {
+                xCoordinateMap.put(x, ++uniqueXCount);
             }
-            treeSize *= 2;
         }
-        for (int i = 1; i <= uniqueYCount; i++) {
-            mainClass.initializeSegmentTree();
-            mainClass.constructSegmentTree();
-            int lastY = i;
-            for (GoldMine goldMine : goldMines) {
-                if (goldMine.y < i) continue;
-                if (goldMine.y == lastY) {
-                    mainClass.updateSegmentTree(goldMine.x - 1, goldMine.w);
-                } else {
-                    maxGold = Math.max(mainClass.getMaxValue(), maxGold);
-                    lastY = goldMine.y;
-                    mainClass.initializeSegmentTree();
-                    mainClass.constructSegmentTree();
-                    mainClass.updateSegmentTree(goldMine.x - 1, goldMine.w);
+
+        // 금광들의 x 좌표를 매핑된 값으로 변경
+        for (int i = 0; i < N; i++) {
+            mines[i].x = xCoordinateMap.get(mines[i].x);
+        }
+
+        // y 좌표를 기준으로 금광 배열을 정렬
+        Arrays.sort(mines, Comparator.comparingInt(mine -> mine.y));
+
+        // y 좌표가 같은 금광들을 그룹으로 분할
+        int prevY = Integer.MIN_VALUE;
+        for (Mine mine : mines) {
+            if (mine.y != prevY) {
+                mineLists.add(new ArrayList<>());
+            }
+            mineLists.get(mineLists.size() - 1).add(mine);
+            prevY = mine.y;
+        }
+
+        // 세그먼트 트리를 초기화
+        segmentTree = new SegmentTreeNode[4 * uniqueXCount];
+
+        // 각 y 좌표 그룹에 대해 최대 금의 양을 계산합니다.
+        for (int start = 0; start < mineLists.size(); start++) {
+            Arrays.fill(segmentTree, DUMMY_NODE);
+            for (int end = start; end < mineLists.size(); end++) {
+                for (Mine mine : mineLists.get(end)) {
+                    updateSegmentTree(1, 1, uniqueXCount, mine.x, mine.weight);
                 }
+                maxGold = Math.max(maxGold, segmentTree[1].max);
             }
-            maxGold = Math.max(mainClass.getMaxValue(), maxGold);
         }
+
         System.out.println(maxGold);
+    }
+
+    // 세그먼트 트리를 업데이트
+    static void updateSegmentTree(int node, int start, int end, int target, long value) {
+        if (target > end || target < start) {
+            return;
+        }
+        if (start == target && target == end) {
+            segmentTree[node] = new SegmentTreeNode(segmentTree[node].max + value);
+            return;
+        }
+        int mid = (start + end) / 2;
+        updateSegmentTree(node * 2, start, mid, target, value);
+        updateSegmentTree(node * 2 + 1, mid + 1, end, target, value);
+        segmentTree[node] = mergeNodes(segmentTree[node * 2], segmentTree[node * 2 + 1]);
+    }
+
+    // 두 노드를 병합 -> 새로운 노드를 생성
+    static SegmentTreeNode mergeNodes(SegmentTreeNode left, SegmentTreeNode right) {
+        return new SegmentTreeNode(
+            Math.max(left.leftMax, left.sum + right.leftMax),
+            Math.max(right.rightMax, right.sum + left.rightMax),
+            Math.max(Math.max(left.max, right.max), left.rightMax + right.leftMax),
+            left.sum + right.sum
+        );
     }
 }
