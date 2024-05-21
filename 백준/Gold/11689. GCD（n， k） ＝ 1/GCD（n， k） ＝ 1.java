2123ec1e5651a751ed42bf0835066f2baede8e68
yullgii <142703323+jinyr1128@users.noreply.github.com>
2024-05-21 11:46:41 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 224 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        System.out.println(eulerTotient(n));
+    }
+
+    public static long eulerTotient(long n) {
+        long result = n;
+        for (long p = 2; p * p <= n; p++) {
+            if (n % p == 0) {
+                while (n % p == 0) {
+                    n /= p;
+                }
+                result -= result / p;
+            }
+        }
+        if (n > 1) {
+            result -= result / n;
+        }
+        return result;
+    }
+}
