64ee938052d478635298fbe5b8d5f8712a6165d6
yullgii <142703323+jinyr1128@users.noreply.github.com>
2024-06-16 09:59:36 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 564 ms, Memory: 21768 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        scanner.close();
+
+        // 상단 삼각형 부분
+        for (int i = 0; i < N; i++) {
+            // 공백 출력
+            for (int j = 0; j < N - i - 1; j++) {
+                System.out.print(" ");
+            }
+            // 별 출력
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            // 줄바꿈
+            System.out.println();
+        }
+
+        // 하단 삼각형 부분
+        for (int i = N - 2; i >= 0; i--) {
+            // 공백 출력
+            for (int j = 0; j < N - i - 1; j++) {
+                System.out.print(" ");
+            }
+            // 별 출력
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            // 줄바꿈
+            System.out.println();
+        }
+    }
+}
