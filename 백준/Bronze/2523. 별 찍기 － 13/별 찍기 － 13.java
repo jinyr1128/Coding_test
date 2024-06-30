81c1c52530dc2356ae2a4eeb8cbc62babac1f7d9
yullgii <142703323+jinyr1128@users.noreply.github.com>
2024-07-01 07:43:26 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 308 ms, Memory: 22248 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // 입력 받기
+        int N = scanner.nextInt();
+
+        // 첫 번째 패턴 (1행부터 N행까지)
+        for (int i = 1; i <= N; i++) {
+            // 별 출력
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // 두 번째 패턴 (N+1행부터 2*N-1행까지)
+        for (int i = N - 1; i > 0; i--) {
+            // 별 출력
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
