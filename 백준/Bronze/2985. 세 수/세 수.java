acf1e8df2cdab6fa2a5764060c7865853ff62439
yullgii <142703323+jinyr1128@users.noreply.github.com>
2024-07-25 00:20:50 +0900

    [Bronze III] Title: 세 수, Time: 240 ms, Memory: 18592 KB -BaekjoonHub

---
+++ 백준/Bronze/2985. 세 수/세 수.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        scanner.close();
+
+        if (a + b == c) {
+            System.out.println(a + "+" + b + "=" + c);
+        } else if (a - b == c) {
+            System.out.println(a + "-" + b + "=" + c);
+        } else if (a * b == c) {
+            System.out.println(a + "*" + b + "=" + c);
+        } else if (b != 0 && a / b == c) {
+            System.out.println(a + "/" + b + "=" + c);
+        } else if (a == b + c) {
+            System.out.println(a + "=" + b + "+" + c);
+        } else if (a == b - c) {
+            System.out.println(a + "=" + b + "-" + c);
+        } else if (a == b * c) {
+            System.out.println(a + "=" + b + "*" + c);
+        } else if (c != 0 && a == b / c) {
+            System.out.println(a + "=" + b + "/" + c);
+        }
+    }
+}
