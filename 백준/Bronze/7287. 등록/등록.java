2ea7d6a939bb83be934e8ca71af1b3c1e18f9176
yullgii <142703323+jinyr1128@users.noreply.github.com>
2024-09-14 00:14:12 +0900

    [Bronze V] Title: 등록, Time: 92 ms, Memory: 13900 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // 첫 번째 줄: 맞은 문제의 수
+        System.out.println("401");
+        // 두 번째 줄: 아이디 (자신의 아이디로 수정)
+        System.out.println("jinyr1128");
+    }
+}
