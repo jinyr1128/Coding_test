656bde0f91e5a7a101d5c25e7bda8161fcce159a
yullgii <142703323+jinyr1128@users.noreply.github.com>
2024-11-22 08:58:13 +0900

    [Platinum III] Title: 다각형 게임, Time: 124 ms, Memory: 13640 KB -BaekjoonHub

--- 백준/Platinum/13034. 다각형 게임/다각형 게임.java
+++ 백준/Platinum/13034. 다각형 게임/다각형 게임.java
@@ -5,26 +5,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
-        // DP 배열 (Grundy 수 계산)
         int[] DP = new int[n + 1];
-
         for (int i = 2; i <= n; i++) {
-            // Set을 사용해 현재 Grundy 수를 계산
             Set<Integer> grundySet = new HashSet<>();
             for (int j = 1; j < i; j++) {
-                grundySet.add(DP[j - 1] ^ DP[i - 1 - j]); // Nim-Sum 계산
+                grundySet.add(DP[j - 1] ^ DP[i - 1 - j]);
             }
-
-            // 현재 Grundy 수 찾기
             int grundy = 0;
             while (grundySet.contains(grundy)) {
                 grundy++;
             }
             DP[i] = grundy;
         }
-
-        // Grundy 수가 0이면 홍준이 승리, 그렇지 않으면 성관이 승리
         System.out.println(DP[n] != 0 ? 1 : 2);
     }
 }
